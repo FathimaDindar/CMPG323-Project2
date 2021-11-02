9567091f62298d22493151a101a2d2a816ab2d5b
FathimaDindar <fathimadindar01@gmail.com>
2021-11-02 19:42:47 +0200

    Add project files.

---
+++ StayConnected/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StayConnected.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
