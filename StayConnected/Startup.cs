c04c5da4c3e967e25edd80d391e979a0cf8c9084
FathimaDindar <fathimadindar01@gmail.com>
2021-11-18 07:48:31 +0200

    IdentityServer

--- StayConnected/Startup.cs
+++ StayConnected/Startup.cs
@@ -24,6 +24,12 @@ namespace StayConnected
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddAuthentication("Bearer")
+                .AddIdentityServerAuthentication("Bearer", options =>
+                {
+                    options.ApiName = "myApi";
+                    options.Authority = "https://localhost:44322";
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +50,7 @@ namespace StayConnected
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
